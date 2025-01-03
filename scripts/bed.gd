c0106dce666823c968ed32580ac88cd447e9e2db
marcovcode <marcov.code@gmail.com>
2025-01-03 15:11:41 +0100

    Style bottom dialogue box

--- scripts/bed.gd
+++ scripts/bed.gd
@@ -4,4 +4,4 @@ func _physics_process(delta):
     for body in get_overlapping_bodies():
         if body.is_in_group("charlotte") and Input.is_action_just_pressed("interact") and GlobalVariables.can_interact:
             SaveSystem.save_game()
-            # Dialogic.start("game_saved")
+            Dialogic.start("game_saved")
