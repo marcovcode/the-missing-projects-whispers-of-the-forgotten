a22f7e321a4074a10c14eb6c1728d393aa997a7f
marcovcode <marcov.code@gmail.com>
2025-01-03 11:34:44 +0100

    Style full_screen dialogic

--- scripts/bedroom.gd
+++ scripts/bedroom.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+func _on_timeline_started():
+    Dialogic.Inputs.set_block_signals(true)
+
+func _on_timeline_ended():
+    Dialogic.Inputs.set_block_signals(false)
+
 func _ready():
+    Dialogic.timeline_started.connect(_on_timeline_started)
+    Dialogic.timeline_ended.connect(_on_timeline_ended)
+
     if GlobalVariables.game_state["time_passage"] == "a_day_in_december":
         Dialogic.start("a_day_in_december")
