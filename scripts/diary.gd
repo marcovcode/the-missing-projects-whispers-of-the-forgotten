527609bd63c4916b1c99bdfb158aa95f3fa5888a
marcovcode <marcov.code@gmail.com>
2025-01-03 19:53:40 +0100

    Add wardrobe and remove diary save

--- scripts/diary.gd
+++ scripts/diary.gd
@@ -3,5 +3,4 @@ extends Area2D
 func _physics_process(delta):
     for body in get_overlapping_bodies():
         if body.is_in_group("charlotte") and Input.is_action_just_pressed("interact") and GlobalVariables.can_interact:
-            SaveSystem.save_game()
             Dialogic.start("forgot_diary_code")
