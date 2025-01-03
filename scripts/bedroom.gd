b183f04abb00b5df9aee6c786a2eff5957d855b4
marcovcode <marcov.code@gmail.com>
2025-01-03 15:17:50 +0100

    Fix interact

--- scripts/bedroom.gd
+++ scripts/bedroom.gd
@@ -1,15 +1,12 @@
 extends Node2D
 
-func _on_timeline_started():
-    Dialogic.Inputs.set_block_signals(true)
-
 func _on_timeline_ended():
     Dialogic.Inputs.set_block_signals(false)
 
 func _ready():
-    Dialogic.timeline_started.connect(_on_timeline_started)
     Dialogic.timeline_ended.connect(_on_timeline_ended)
 
     if GlobalVariables.game_state["time_passage"] == "a_day_in_december" and not GlobalVariables.game_state["dialogues"]["has_shown_a_day_in_december"]:
-        Dialogic.start("a_day_in_december")
+        Dialogic.Inputs.set_block_signals(true)
         GlobalVariables.game_state["dialogues"]["has_shown_a_day_in_december"] = true
+        Dialogic.start("a_day_in_december")
