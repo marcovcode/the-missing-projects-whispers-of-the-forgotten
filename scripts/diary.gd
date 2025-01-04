4447a023c4a567552be7c83828a195d886f8c8ed
marcovcode <marcov.code@gmail.com>
2025-01-04 17:22:03 +0100

    Add wrote_diary_code_somewhere

--- scripts/diary.gd
+++ scripts/diary.gd
@@ -3,8 +3,11 @@ extends Area2D
 func _physics_process(delta):
     for body in get_overlapping_bodies():
         if body.is_in_group("charlotte") and Input.is_action_just_pressed("interact") and GlobalVariables.can_interact:
-            if GlobalVariables.game_state["story_progress"]["a_day_in_december"]["knows_diary_code"]:
-                Dialogic.start("insert_diary_code")
+            if GlobalVariables.game_state["story_progress"]["a_day_in_december"]["has_checked_diary"]:
+                if GlobalVariables.game_state["story_progress"]["a_day_in_december"]["knows_diary_code"]:
+                    Dialogic.start("insert_diary_code")
+                else:
+                    Dialogic.start("wrote_diary_code_somewhere")
             else:
                 GlobalVariables.game_state["story_progress"]["a_day_in_december"]["has_checked_diary"] = true
                 Dialogic.start("forgot_diary_code")
