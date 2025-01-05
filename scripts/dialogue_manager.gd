08cedae9192edb5a01ecd50f8ad1c1930bd91cbc
marcovcode <marcov.code@gmail.com>
2025-01-05 12:45:20 +0100

    Go to dhow diary page

--- scripts/dialogue_manager.gd
+++ scripts/dialogue_manager.gd
@@ -9,6 +9,11 @@ func _on_timeline_started():
 func _on_timeline_ended():
     can_interact = true
 
+func _on_timeline_signal(argument):
+    if argument == "show_diary_page":
+        get_tree().change_scene_to_file("res://scenes/show_diary_page.tscn")
+
 func _ready():
     Dialogic.timeline_started.connect(_on_timeline_started)
     Dialogic.timeline_ended.connect(_on_timeline_ended)
+    Dialogic.signal_event.connect(_on_timeline_signal)
