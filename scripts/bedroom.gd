5a8584fb3c6c994abe846881e514d59131b9355b
marcovcode <marcov.code@gmail.com>
2025-01-05 12:07:10 +0100

    Code cleanup

--- scripts/bedroom.gd
+++ scripts/bedroom.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
-var story_progress:
-    get: return GlobalVariables.game_state["story_progress"]
-var a_day_in_december_progress:
-    get: return GlobalVariables.game_state["story_progress"]["a_day_in_december"]
+var current_chapter:
+    get: return GlobalVariables.game_state["story_progress"]["current_chapter"]
+var has_shown_woke_up:
+    get: return GlobalVariables.game_state["story_progress"]["a_day_in_december"]["has_shown_woke_up"]
+    set(value): GlobalVariables.game_state["story_progress"]["a_day_in_december"]["has_shown_woke_up"] = value
 
 func _ready():
-    if story_progress["current_chapter"] == "a_day_in_december":
-        if not a_day_in_december_progress["has_shown_woke_up"]:
-            a_day_in_december_progress["has_shown_woke_up"] = true
+    if current_chapter == "a_day_in_december":
+        if not has_shown_woke_up:
+            has_shown_woke_up = true
             Dialogic.start("woke_up")
--- scripts/diary.gd
+++ scripts/diary.gd
@@ -1,11 +1,14 @@
 extends Area2D
 
+var can_interact:
+    get: return GlobalVariables.can_interact
+
 var a_day_in_december_progress:
     get: return GlobalVariables.game_state["story_progress"]["a_day_in_december"]
 
 func _physics_process(delta):
     for body in get_overlapping_bodies():
-        if body.is_in_group("charlotte") and Input.is_action_just_pressed("interact") and GlobalVariables.can_interact:
+        if body.is_in_group("charlotte") and Input.is_action_just_pressed("interact") and can_interact:
             if a_day_in_december_progress["has_checked_diary"]:
                 if a_day_in_december_progress["knows_diary_code"]:
                     Dialogic.start("insert_diary_code")
