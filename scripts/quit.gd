043b2ce58fabd50f679bc4f478dc814dfb485577
marcovcode <marcov.code@gmail.com>
2025-01-21 19:52:43 +0100

    Add sound to buttons

--- scripts/quit.gd
+++ scripts/quit.gd
@@ -1,9 +1,18 @@
 extends Button
 
+@onready var hover_sound = $HoverSound
+@onready var pressed_sound = $PressedSound
+
 func _ready():
+    mouse_entered.connect(_on_button_mouse_entered)
     pressed.connect(_on_button_pressed)
 
+func _on_button_mouse_entered():
+    hover_sound.play()
+
 func _on_button_pressed():
+    pressed_sound.play()
+
     Transition.transition()
     await Transition.transition_finished
     get_tree().quit()
--- scripts/start.gd
+++ scripts/start.gd
@@ -1,14 +1,23 @@
 extends Button
 
+@onready var hover_sound = $HoverSound
+@onready var pressed_sound = $PressedSound
+
 var story_progress:
     get: return GlobalVariables.game_state["story_progress"]
 var current_chapter:
     get: return GlobalVariables.game_state["story_progress"]["current_chapter"]
 
 func _ready():
+    mouse_entered.connect(_on_button_mouse_entered)
     pressed.connect(_on_button_pressed)
 
+func _on_button_mouse_entered():
+    hover_sound.play()
+
 func _on_button_pressed():
+    pressed_sound.play()
+
     var target_scene = "res://scenes/show_current_chapter.tscn"
 
     if story_progress[current_chapter]["has_shown_chapter_name"]:
