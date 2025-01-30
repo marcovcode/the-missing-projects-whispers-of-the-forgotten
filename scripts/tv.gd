914bd2c87e1e14e78a23d7effdee36e6ac9dcbaa
marcovcode <marcov.code@gmail.com>
2025-01-30 15:20:20 +0100

    Add flashing colors dialogue

--- scripts/tv.gd
+++ scripts/tv.gd
@@ -4,8 +4,15 @@ extends StaticBody2D
 
 var can_interact:
     get: return GlobalVariables.can_interact
+var current_chapter:
+    get: return GlobalVariables.game_state["story_progress"]["current_chapter"]
+var has_seen_colors:
+    get: return GlobalVariables.game_state["story_progress"]["a_week_later"]["has_seen_colors"]
+    set(value): GlobalVariables.game_state["story_progress"]["a_week_later"]["has_seen_colors"] = value
 
 func _physics_process(delta):
     if raycast.is_colliding() and raycast.get_collider() == self:
         if Input.is_action_just_pressed("interact") and can_interact:
-            Dialogic.start("tv")
+            if current_chapter == "a_week_later":
+                Dialogic.start("flashing_colors")
+                has_seen_colors = true
